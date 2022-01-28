e5be769e825eebc81b4561ac1b55f493b0d391d1
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-28 19:47:16 +0800

    added mongoose connection

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const fastify = require("fastify")({ logger: true });
+const mongoose = require("mongoose");
+require("dotenv").config({});
 
 fastify.register(require("./routes/test.route"));
+fastify.register(require("./routes/auth.route"));
+
 const PORT = process.env.PORT || 3000;
 const startApp = async () => {
   try {
     await fastify.listen(PORT);
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+    });
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
