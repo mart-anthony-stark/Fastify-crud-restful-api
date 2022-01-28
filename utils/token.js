d47665b282c76bb73b846a05946b4b024f08750c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-28 20:00:43 +0800

    added auth checker

--- utils/token.js
+++ utils/token.js
@@ -4,4 +4,20 @@ const createToken = (user) => {
   return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
 
-module.exports = { createToken };
+const isAuth = (req, reply, next) => {
+  if (!req.headers.token)
+    return reply.code(401).send({ error: "Unauthenticated" });
+
+  const token = req.headers.token.split(" ")[1];
+  if (!token) return reply.code(401).send({ error: "Unauthorized" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    if (err) return reply.code(401).send({ error: "Invalid token" });
+
+    decodedToken.user.password = undefined;
+    req.user = decodedToken.user;
+    next();
+  });
+};
+
+module.exports = { createToken, isAuth };
