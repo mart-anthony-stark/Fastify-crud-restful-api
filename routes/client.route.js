91ee099dc4d7c7405c6926da32390497750a6821
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-28 20:22:09 +0800

    added auth prehandler to client route

--- routes/client.route.js
+++ routes/client.route.js
@@ -7,10 +7,26 @@ const clientRoutes = (fastify, options, done) => {
     { preHandler: [isAuth] },
     clientController.getClients
   );
-  fastify.get("/clients/:id", clientController.getClientById);
-  fastify.post("/clients", clientController.createClient);
-  fastify.put("/clients", clientController.updateClient);
-  fastify.delete("/clients", clientController.deleteClient);
+  fastify.get(
+    "/clients/:id",
+    { preHandler: [isAuth] },
+    clientController.getClientById
+  );
+  fastify.post(
+    "/clients",
+    { preHandler: [isAuth] },
+    clientController.createClient
+  );
+  fastify.put(
+    "/clients",
+    { preHandler: [isAuth] },
+    clientController.updateClient
+  );
+  fastify.delete(
+    "/clients",
+    { preHandler: [isAuth] },
+    clientController.deleteClient
+  );
   done();
 };
 module.exports = clientRoutes;
